3e29fe76f4192d0fad9d930159a68471924a9c28
Semen Pavelko <se.pa.94@mail.ru>
2023-05-04 16:44:56 +0300

    fix: исправление табулации, версия 1.01

--- src/main/java/ru/yandex/practicum/filmorate/dao/EntityManagerPool.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/EntityManagerPool.java
@@ -4,14 +4,15 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-
 // Так как сложно было настроить это в контексте спринга, сделал синглетон.
 public class EntityManagerPool {
     private static final EntityManagerFactory entityManagerFactory = Persistence
             .createEntityManagerFactory("persistenceUnit");
-    private EntityManagerPool(){}
 
-    public static EntityManager getEntityManager(){
+    private EntityManagerPool() {
+    }
+
+    public static EntityManager getEntityManager() {
         return entityManagerFactory.createEntityManager();
     }
 }
