8a4ba8f20e1cb99c30d36908b780274ab7615da7
Semen Pavelko <se.pa.94@mail.ru>
2023-06-21 15:55:37 +0300

    fix: исправление sql топ фильмов пользователя, версия 6.03

--- src/main/java/ru/yandex/practicum/filmorate/utils/FilmsSQL.java
+++ src/main/java/ru/yandex/practicum/filmorate/utils/FilmsSQL.java
@@ -58,7 +58,9 @@ public final class FilmsSQL {
             "                         , COUNT(user_id) as count_likes\n" +
             "                    FROM likes\n" +
             "                    GROUP BY film_id) cf\n" +
-            "                   ON cf.film_id = f.film_id and user_id = ?\n" +
+            "                   ON cf.film_id = f.film_id\n" +
+            "         JOIN likes l\n" +
+            "                   ON f.film_id = l.film_id AND user_id = ?\n" +
             joinFilmAttribute +
             "ORDER BY count_likes desc";
 
