8bb7fdcbb760b207a6398b3cc89fd29be2404ef6
Rustam N <charlsetarleton@yandex.ru>
2023-06-22 12:21:48 +0300

    feat: добавить цепочку для режиссеров, включая методы создания, обновления, удаления, получения режиссеров; добавить новый метод в цепочку фильмов; fix: исправить парсинг фильмов с учетом режиссеров в обе стороны; добавить в SQL запросы режиссеров

--- src/main/java/ru/yandex/practicum/filmorate/service/directorservice/DirectorService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/directorservice/DirectorService.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.service.directorservice;
 import ru.yandex.practicum.filmorate.model.Director;
 
 import java.util.List;
+
 public interface DirectorService {
 
     Director getDirector(long id);
