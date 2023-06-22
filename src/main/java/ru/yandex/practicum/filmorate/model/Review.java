812582a7e59e92f25f3f3b3554c64a32d0a95be4
TimurDV88 <kumba-yo@yandex.ru>
2023-06-22 12:17:09 +0300

    fix: исправлен класс Review 2.4

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -1,34 +1,24 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
-@ToString
+@Data
 public class Review {
-
-    @Getter
-    @Setter
     private Long reviewId;
-    @Getter
-    @Setter
     @NotNull
     private String content;
     @NotNull
     private Boolean isPositive;
-    @Getter
-    @Setter
     @NotNull
     private Long userId;
-    @Getter
-    @Setter
     @NotNull
     private Long filmId;
-    @Getter
-    @Setter
     private Integer useful = 0;
 
     public Review(Long reviewId, String content, Boolean isPositive, Long userId, Long filmId) {
@@ -37,13 +27,5 @@ public class Review {
         this.isPositive = isPositive;
         this.userId = userId;
         this.filmId = filmId;
-    }
-
-    public boolean getIsPositive() {
-        return isPositive;
-    }
-
-    public void setIsPositive(boolean isPositive) {
-        this.isPositive = isPositive;
     }
 }
